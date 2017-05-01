6af515554936b522aff78c4833c723b22602977f
andy <andy@andydote.co.uk>
2017-05-01 16:30:11 +0300

    lambda: use 6.10 runtime

--- infrastructure/api/lambda.tf
+++ infrastructure/api/lambda.tf
@@ -9,7 +9,7 @@ resource "aws_lambda_function" "crowbar_api_event_lambda" {
   function_name = "crowbar_api_event"
   role = "${aws_iam_role.crowbar_lambda_role.arn}"
   handler = "eventapi.handler"
-  runtime = "nodejs4.3"
+  runtime = "nodejs6.10"
   source_code_hash = "${base64sha256(file("${data.archive_file.api_lambda.output_path}"))}"
   publish = true
 }
