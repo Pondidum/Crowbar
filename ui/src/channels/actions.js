445d76e965fedb76201bf59f9e7c456680413b0c
andy <andy@andydote.co.uk>
2017-05-06 21:01:33 +0300

    ui: dispatch channel creation to api, handle responses

--- ui/src/channels/actions.js
+++ ui/src/channels/actions.js
@@ -1,15 +1,33 @@
-export const viewChannel = (channelName) => {
+import { CALL_API } from 'redux-api-middleware'
+import uuid from 'uuid/v4'
+
+export const viewChannel = channelName => {
   return {
     type: 'VIEW_CHANNEL',
     channelName
-  };
+  }
 }
 
 export const createChannel = (userId, channelName, channelDescription) => {
-  return {
-    type: 'CREATE_CHANNEL',
-    userId,
+  const event = {
+    eventId: uuid(),
+    timestamp: new Date().getTime(),
+    channelId: uuid(),
     channelName,
-    channelDescription
+    channelDescription,
+    userId
+  }
+
+  return {
+    [CALL_API]: {
+      endpoint: 'https://9a0hixeit8.execute-api.eu-west-1.amazonaws.com/api/events',
+      method: 'POST',
+      body: JSON.stringify(event),
+      types: [
+        { type: 'CREATE_CHANNEL_REQUEST', payload: event },
+        { type: 'CREATE_CHANNEL_SUCCESS', payload: event },
+        { type: 'CREATE_CHANNEL_FAILURE', payload: event }
+      ]
+    }
   }
 }
--- ui/src/channels/reducers.js
+++ ui/src/channels/reducers.js
@@ -1,10 +1,24 @@
-
-const createChannel = (action) => {
+const createChannel = (action, status = '') => {
   return {
+    id: action.channelId,
     name: action.channelName,
     description: action.channelDescription,
-    creator: action.userId
+    creator: action.userId,
+    status: status
   }
+}
+
+const replaceChannel = (available, channel) => {
+  let index = available.findIndex(c => c.channelId === channel.channelId)
+
+  if (index < 0) {
+    index = available.length
+  }
+
+  const before = available.slice(0, index)
+  const after = available.slice(index + 1, available.length)
+
+  return before.concat([channel]).concat(after)
 }
 
 const defaultState = {
@@ -13,22 +27,37 @@ const defaultState = {
 }
 
 export default (state = defaultState, action) => {
-
   switch (action.type) {
-
-    case "VIEW_CHANNEL":
+    case 'VIEW_CHANNEL':
       return Object.assign({}, state, {
         selectedChannel: action.channelName
-      });
+      })
 
-    case 'CREATE_CHANNEL':
-      const available = state.available.concat([ createChannel(action) ]);
+    case 'CREATE_CHANNEL_REQUEST': {
+      const channel = createChannel(action.payload, 'PENDING')
 
       return Object.assign({}, state, {
-        available: available
-      });
+        available: replaceChannel(state.available, channel)
+      })
+    }
+
+    case 'CREATE_CHANNEL_SUCCESS': {
+      const channel = createChannel(action.payload)
+
+      return Object.assign({}, state, {
+        available: replaceChannel(state.available, channel)
+      })
+    }
+
+    case 'CREATE_CHANNEL_FAILURE': {
+      const channel = createChannel(action.payload, 'ERROR')
+
+      return Object.assign({}, state, {
+        available: replaceChannel(state.available, channel)
+      })
+    }
 
     default:
-      return state;
+      return state
   }
 }
