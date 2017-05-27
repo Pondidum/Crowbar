3eea50952384c6d084000a93b461026885a022dc
andy <andy@andydote.co.uk>
2017-05-27 22:14:53 +0300

    channel: use new updateView function

--- infrastructure/js/aggregates/channel.js
+++ infrastructure/js/aggregates/channel.js
@@ -1,10 +1,46 @@
-const appendToObject = require('./s3append')
+const update = require('./updateView')
+const updateView = (viewName, id, callback) =>
+  update({
+    viewName,
+    id,
+    callback
+  })
 
-const handles = eventType => eventType === 'CREATE_CHANNEL'
+const handleCreateChannel = event => {
+  const channel = {
+    id: event.channelId,
+    name: event.channelName,
+    description: event.channelDescription
+  }
+
+  updateView('CHANNEL', event.channelId, view => {
+    return Object.assign({}, channel, { users: [] })
+  })
+
+  updateView('ALL_CHANNELS', null, view => {
+    return Object.assign({}, channel, { users: 0 })
+  })
+}
+
+const handleUserJoinedChannel = event => {
+  updateView('CHANNEL', event.channelId, view => {
+    view.users.push(userView[event.userId].name)
+  })
+
+  updateView('ALL_CHANNELS', null, view => {
+    view[event.channelId].users += 1
+  })
+}
 
 exports.handler = (event, context) => {
-  if (!handles(event.type)) {
-    return
+  switch (event.type) {
+    case 'CREATE_CHANNEL': {
+      return handleCreateChannel(event)
+    }
+
+    case 'USER_JOINED_CHANNEL': {
+      return handleUserJoinedChannel(event)
+    }
   }
 
   return appendToObject('events/views/allchannels.json', content => {
