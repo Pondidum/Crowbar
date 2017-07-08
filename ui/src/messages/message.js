ea747684c5fdfbf9c3ab665ef98625d49b5c142b
andy <andy@andydote.co.uk>
2017-07-08 21:56:21 +0300

    messages: add moment and display a timestamp

--- ui/src/messages/message.js
+++ ui/src/messages/message.js
@@ -1,13 +1,21 @@
 import React from 'react'
+import { Col } from 'react-bootstrap'
+import moment from 'moment'
 
 const Message = ({ message }) => {
   const style = {}
+
   if (message.status === 'FAILED') style.color = 'red'
   if (message.status === 'PENDING') style.color = '#999'
 
   return (
-    <li style={style}>
-      {message.text}
+    <li className="row" style={style}>
+      <Col sm={2}>
+        {moment(message.timestamp).fromNow()}
+      </Col>
+      <Col sm={10}>
+        {message.text}
+      </Col>
     </li>
   )
 }
