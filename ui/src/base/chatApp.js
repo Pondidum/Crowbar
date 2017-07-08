7b5b768c9c2b6f7913f238626b477ed210b136be
andy <andy@andydote.co.uk>
2017-07-08 21:40:31 +0300

    ui: move the chatbox into the messages directory

--- ui/src/base/chatApp.js
+++ ui/src/base/chatApp.js
@@ -3,14 +3,13 @@ import { Route } from 'react-router'
 
 import SideBar from '../sidebar'
 import Messages from '../messages'
-import ChatBox from '../chatbox'
+import ChatBox from '../messages/chatbox'
 
-const ChatApp = () => (
+const ChatApp = () =>
   <div className="row">
     <Route path="/chat/:channel?" component={SideBar} />
     <Route path="/chat/:channel?" component={Messages} />
     <ChatBox />
   </div>
-)
 
 export default ChatApp
--- ui/src/messages/actions.js
+++ ui/src/messages/actions.js
@@ -1,5 +1,6 @@
 import { CALL_API } from 'redux-api-middleware'
-import { VIEWS_URL } from '../constants'
+import { VIEWS_URL, API_URL } from '../constants'
+import uuid from 'uuid/v4'
 
 export const listAllMessages = () => {
   return {
@@ -10,6 +11,31 @@ export const listAllMessages = () => {
         'LIST_ALL_MESSAGES_REQUEST',
         'LIST_ALL_MESSAGES_SUCCESS',
         'LIST_ALL_MESSAGES_FAILURE'
+      ]
+    }
+  }
+}
+
+export const sendMessage = (userId, channelId, message) => {
+  const event = {
+    eventId: uuid(),
+    type: 'MESSAGE_SENT',
+    timestamp: new Date().getTime(),
+    messageId: uuid(),
+    userId,
+    channelId,
+    message
+  }
+
+  return {
+    [CALL_API]: {
+      endpoint: API_URL,
+      method: 'POST',
+      body: JSON.stringify(event),
+      types: [
+        { type: 'SEND_MESSAGE_REQUEST', payload: event },
+        { type: 'SEND_MESSAGE_SUCCESS', payload: event },
+        'SEND_MESSAGE_FAILURE'
       ]
     }
   }
