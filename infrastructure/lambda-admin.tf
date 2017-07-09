028aa827db9467f169fba58f81218e6dc55bc2cd
andy <andy@andydote.co.uk>
2017-07-09 22:26:06 +0300

    infra: make the admin lambdas run under separate policies and roles

--- infrastructure/lambda-admin.tf
+++ infrastructure/lambda-admin.tf
@@ -7,7 +7,7 @@ data "archive_file" "lambda_admin" {
 resource "aws_lambda_function" "crowbar_admin_clear" {
   filename = "${data.archive_file.lambda_admin.output_path}"
   function_name = "crowbar_admin_clear"
-  role = "${aws_iam_role.crowbar_lambda_role.arn}"
+  role = "${aws_iam_role.crowbar_admin_lambda_role.arn}"
   handler = "index.clearViews"
   runtime = "nodejs6.10"
   source_code_hash = "${base64sha256(file("${data.archive_file.lambda_admin.output_path}"))}"
--- infrastructure/permissions.tf
+++ infrastructure/permissions.tf
@@ -1,4 +1,4 @@
-data "template_file" "s3_role_policy" {
+data "template_file" "api_lambda_policy" {
   template = "${file("policies/api-lambda-role-policy.json")}"
   vars {
     bucket_name = "${var.bucket_name}"
@@ -13,5 +13,24 @@ resource "aws_iam_role" "crowbar_lambda_role" {
 resource "aws_iam_role_policy" "crowbar_lambda_role_policy" {
   name = "crowbar_lambda_role_policy"
   role = "${aws_iam_role.crowbar_lambda_role.id}"
-  policy = "${data.template_file.s3_role_policy.rendered}"
+  policy = "${data.template_file.api_lambda_policy.rendered}"
+}
+
+
+data "template_file" "admin_lambda_policy" {
+  template = "${file("policies/admin-lambda-role-policy.json")}"
+  vars {
+    bucket_name = "${var.bucket_name}"
+  }
+}
+
+resource "aws_iam_role" "crowbar_admin_lambda_role" {
+  name = "crowbar_admin_lambda_role"
+  assume_role_policy = "${file("policies/admin-lambda-role.json")}"
+}
+
+resource "aws_iam_role_policy" "crowbar_admin_lambda_role_policy" {
+  name = "crowbar_admin_lambda_role_policy"
+  role = "${aws_iam_role.crowbar_admin_lambda_role.id}"
+  policy = "${data.template_file.admin_lambda_policy.rendered}"
 }
