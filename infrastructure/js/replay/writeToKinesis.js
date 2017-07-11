504ce684a12611da39a56ea0626ad72c246bd34b
andy <andy@andydote.co.uk>
2017-07-11 22:45:12 +0300

    replay: write events to kinesis

--- infrastructure/js/replay/writeToKinesis.js
+++ infrastructure/js/replay/writeToKinesis.js
@@ -1,4 +1,16 @@
 const aws = require('aws-sdk')
 const kinesis = new aws.Kinesis()
 
-module.exports = () => Promise.resolve()
+module.exports = events => {
+  var command = {
+    StreamName: 'crowbar_replay_stream',
+    Records: events.map(event => {
+      return {
+        Data: JSON.stringify(event),
+        PartitionKey: event.eventId // ???
+      }
+    })
+  }
+
+  return kinesis.putRecords(command).promise()
+}
