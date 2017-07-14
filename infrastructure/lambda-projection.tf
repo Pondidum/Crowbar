0dc75e2606a11d879c8aa7e8bcffa0cd961d0812
andy <andy@andydote.co.uk>
2017-07-14 23:34:26 +0300

    infra: use separate policy for projection lambda

--- infrastructure/lambda-projection.tf
+++ infrastructure/lambda-projection.tf
@@ -1,3 +1,22 @@
+data "template_file" "projection_lambda_policy" {
+  template = "${file("policies/projection-lambda-role-policy.json")}"
+  vars {
+    bucket_name = "${var.bucket_name}"
+  }
+}
+
+resource "aws_iam_role" "crowbar_projection_lambda_role" {
+  name = "crowbar_projection_lambda_role"
+  assume_role_policy = "${file("policies/projection-lambda-role.json")}"
+}
+
+resource "aws_iam_role_policy" "crowbar_projection_lambda_role_policy" {
+  name = "crowbar_projection_lambda_role_policy"
+  role = "${aws_iam_role.crowbar_projection_lambda_role.id}"
+  policy = "${data.template_file.projection_lambda_policy.rendered}"
+}
+
+
 data "archive_file" "lambda_projection" {
   type = "zip"
   source_dir = "./js/projection"
@@ -7,7 +26,7 @@ data "archive_file" "lambda_projection" {
 resource "aws_lambda_function" "crowbar_projections" {
   filename = "${data.archive_file.lambda_projection.output_path}"
   function_name = "crowbar_projections"
-  role = "${aws_iam_role.crowbar_lambda_role.arn}"
+  role = "${aws_iam_role.crowbar_projection_lambda_role.arn}"
   handler = "index.handler"
   runtime = "nodejs6.10"
   source_code_hash = "${base64sha256(file("${data.archive_file.lambda_projection.output_path}"))}"
